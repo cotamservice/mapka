8b3f15c64804e4f5ddad18b8c6764d1ca0b8fcdb
AntonMiracle <b.anton.m.1986@gmail.com>
2020-05-29 09:26:21 +0200

    update paths

--- js/init.js
+++ js/init.js
@@ -10,8 +10,17 @@ $(function(){
   var callOnHoverOut = function(obj){
       console.log('was hover out: ' + obj.firstId + ';'+obj.secondId);
   };
+  // ONLY FOR calculate correct proportion
+  // var mapka0 = new Mapka0('map0', paths['gb'].regions, callOnClick,callOnHoverIn,callOnHoverOut);
+  // mapka0.generation();
+  ////////////////////////////////////////
 
-  var mapka = new Mapka('map',paths, callOnClick,callOnHoverIn,callOnHoverOut);
+  // var mapka = new Mapka('map',paths, callOnClick,callOnHoverIn,callOnHoverOut);
   // var mapka = new Mapka('map', paths['iceland'].regions, callOnClick,callOnHoverIn,callOnHoverOut);
+  var mapka = new Mapka('map1', paths, callOnClick,callOnHoverIn,callOnHoverOut);
+  mapka.generation();
+  var mapka = new Mapka('map2', paths['gb'].regions, callOnClick,callOnHoverIn,callOnHoverOut);
+  mapka.generation();
+  var mapka = new Mapka('map3', paths['ireland'].regions, callOnClick,callOnHoverIn,callOnHoverOut);
   mapka.generation();
 });
